02cfec2ac36043fa8f5c215677c1bdb0d954a7a9
Naatty123 <nattynengeda@gmail.com>
2022-07-13 12:54:00 +0300

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { Navbar } from 'flowbite-react'
+import React from 'react'
+import logo from '../img/logo.jpg'
+
+export default function Header() {
+  return (
+    <header className='border border-gray fixed w-full shadow-lg z-40'>
+    <Navbar className='bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800'
+    >
+        <Navbar.Brand href="https://flowbite.com/">
+            <img
+            src={logo}
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                   Fitsum Advert     
+            </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+            <button className='text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800'>
+            Get started
+            </button>
+            <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+            <Navbar.Link
+            href="/navbars"
+            active={true}
+            >
+            Home
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+            About
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+            Services
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+            Pricing
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">
+            Contact
+            </Navbar.Link>
+        </Navbar.Collapse>
+    </Navbar>
+    </header>
+  )
+}
